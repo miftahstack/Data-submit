b441b9deeb56277ba4ad36867194e647f6b28c2a
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-06-17 14:37:44 +0600

    Added card

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let nameInput = document.querySelector(".nameInput");
+let desInput = document.querySelector(".desInput");
+let subbtn = document.querySelector(".subbtn");
+let dataOutput = document.querySelector(".dataOutput");
+
+let arr = [];
+
+function showcard() {
+  dataOutput.innerHTML = "";
+  arr.map((item) => {
+    console.log(item);
+
+    dataOutput.innerHTML += `
+    <div class="card w-96 bg-base-100 border border-base-300 shadow-xl mt-10">
+        <div class="card-body">
+            <h2 class="card-title">${item.name}</h2>
+            <p>${item.description}</p>
+        </div>
+    </div>`;
+  });
+}
+
+subbtn.addEventListener("click", () => {
+  showcard();
+  arr.push({
+    name: nameInput.value,
+    description: desInput.value,
+  });
+  (nameInput.value = ""), (desInput.value = "");
+});
